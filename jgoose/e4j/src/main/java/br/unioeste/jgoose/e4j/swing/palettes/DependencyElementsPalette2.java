8f8ac2c86f8af6c1760422831656ae9314791566
Eduardo <du01pimentel@gmail.com>
2025-06-19 13:28:05 -0300

    Teste3

---
+++ jgoose/e4j/src/main/java/br/unioeste/jgoose/e4j/swing/palettes/DependencyElementsPalette2.java
@@ -0,0 +1,38 @@
+/*    */ package br.unioeste.jgoose.e4j.swing.palettes;
+/*    */ 
+/*    */ import br.unioeste.jgoose.e4j.actions.ImporStencilAction2;
+/*    */ import br.unioeste.jgoose.e4j.filters.ShapeFilenameFilter;
+/*    */ import com.mxgraph.util.mxResources;
+/*    */ import com.mxgraph.util.mxUtils;
+/*    */ import java.io.File;
+/*    */ import java.io.IOException;
+/*    */ import javax.swing.JTabbedPane;
+/*    */ import org.apache.log4j.Logger;
+
+/*    */ public class DependencyElementsPalette2
+/*    */   extends AbstractPalette
+/*    */ {
+/* 23 */   private static final Logger CONSOLE = Logger.getLogger("console");
+/*    */   
+/*    */   public DependencyElementsPalette2(JTabbedPane libraryPane) {
+/* 26 */     super(mxResources.get("Elements", "Dependency Elements"), libraryPane);
+/*    */     
+/* 28 */     File shapesFolder = new File("resources/shapes/elements/");
+/* 29 */     File[] files = shapesFolder.listFiles(ShapeFilenameFilter.instance);
+/* 30 */     if ((files == null) || (files.length < 1)) {
+/* 31 */       CONSOLE.info("no shape found. " + shapesFolder.getAbsolutePath());
+/* 32 */       return;
+/*    */     }
+/* 34 */     for (File f : files) {
+/*    */       try
+/*    */       {
+/* 37 */         String nodeXml = mxUtils.readFile(f.getAbsolutePath());
+/* 38 */         if (!f.getName().matches("actor.shape|actor_agent.shape|actor_position.shape|actor_role.shape|softgoal.shape")) {
+/* 39 */           ImporStencilAction2.addStencilShape(this, nodeXml, f.getParent() + File.separator);
+/*    */         }
+/*    */       } catch (IOException ex) {
+/* 42 */         CONSOLE.fatal(ex);
+/*    */       }
+/*    */     }
+/*    */   }
+/*    */ }
